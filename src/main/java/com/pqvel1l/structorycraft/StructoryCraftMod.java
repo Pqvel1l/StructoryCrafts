f13c629386235464ee1c22b72500296d19f06ba8
Pqvel1l <mebao123@mail.ru>
2024-06-22 18:01:24 +0200

    Upload to Git

---
+++ src/main/java/com/pqvel1l/structorycraft/StructoryCraftMod.java
@@ -0,0 +1,15 @@
+package com.pqvel1l.structorycraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StructoryCraftMod implements ModInitializer {
+	public static final String MODID = "structorycraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/pqvel1l/structorycraft/StructoryCraftModClient.java
@@ -0,0 +1,9 @@
+package com.pqvel1l.structorycraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StructoryCraftModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
